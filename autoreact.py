4cce39a7f533688a6ed27a4a53f6718c07a02707
anon97945 <54719502+anon97945@users.noreply.github.com>
2022-06-22 06:54:43 +0200

    Update autoreact.py

--- autoreact.py
+++ autoreact.py
@@ -1,4 +1,4 @@
-__version__ = (0, 1, 2)
+__version__ = (0, 1, 3)
 
 # ▄▀█ █▄ █ █▀█ █▄ █ █▀█ ▀▀█ █▀█ █ █ █▀
 # █▀█ █ ▀█ █▄█ █ ▀█ ▀▀█   █ ▀▀█ ▀▀█ ▄█
@@ -17,6 +17,8 @@ __version__ = (0, 1, 2)
 
 
 import logging
+import random
+import asyncio
 
 from telethon.tl.types import Message
 from telethon.errors import ReactionInvalidError
@@ -30,25 +32,40 @@ logger = logging.getLogger(__name__)
 class ApodiktumAutoReactMod(loader.Module):
     """
     AutoReact to messages.
+    Check the `.config apodiktum autoreact`
     """
     strings = {
         "name": "Apodiktum AutoReact",
         "developer": "@anon97945",
         "no_command": "<b>This is no command. Everything is configured within</b>\n<code>.config apodiktum autoreact</code>\n\n<b><i><u>This module was created by {}.</u></i></b>",
         "_cfg_doc_raise_error": "Raise an error if the emoji is not valid.",
+        "_cfg_doc_shuffle": "Shuffles the list of given emojis.",
+        "_cfg_doc_global_delay": "The delay between reactions are send in seconds.",
+        "_cfg_doc_random_delay": "Randomizes the delay between reactions. Randomness is between 0 and the global delay.",
+        "_cfg_doc_reactions_chance": ("The chance of reacting to a message.\n0.0 is the chance of not reacting to a message.\n1.0 is the chance of reacting to a message every time."
+                                      "Pattern:\n<userid/all>|<chatid/global>|<percentage(0.00-1)>\n\nExample:\n1234567|global|0.8"),
         "_cfg_doc_reactions": ("Setup AutoReact.\nYou can define alternative emojis to react with, when the Chat doesn't allow the first, second etc.\n"
                                "You can also define an all OR global state, which will either apply reactions to all chat members (all) or to one user in all groups(global).\n"
                                "You can't use both at the same time!\n\n"
-                               "Pattern:\n<userid/all>|<chatid/global>|<emoji1>|<emoji2>|<emoji3>..."),
+                               "Pattern:\n<userid/all>|<chatid/global>|<emoji1>|<emoji2>|<emoji3>...\n\nExample:\nall|1792410946|❤️|👍|🔥"),
     }
 
     def __init__(self):
         self.config = loader.ModuleConfig(
             loader.ConfigValue(
                 "reaction_active",
-                False,
+                True,
                 doc=lambda: self.strings("_cfg_doc_react"),
                 validator=loader.validators.Boolean(),
+            ),
+            loader.ConfigValue(
+                "global_delay",
+                0.5,
+                doc=lambda: self.strings("_cfg_doc_global_delay"),
+                validator=loader.validators.Union(
+                    loader.validators.Float(minimum=0, maximum=600),
+                    loader.validators.NoneType(),
+                ),
             ),
             loader.ConfigValue(
                 "raise_error",
@@ -57,11 +74,31 @@ class ApodiktumAutoReactMod(loader.Module):
                 validator=loader.validators.Boolean(),
             ),
             loader.ConfigValue(
+                "random_delay",
+                False,
+                doc=lambda: self.strings("_cfg_doc_random_delay"),
+                validator=loader.validators.Boolean(),
+            ),
+            loader.ConfigValue(
                 "reactions",
+                ["all|1792410946|❤️|👍|🔥"],
                 doc=lambda: self.strings("_cfg_doc_reactions"),
                 validator=loader.validators.Series(
                     validator=loader.validators.RegExp(r"^(?:(?:\d+)[|](?:\d+|global)|(?:\d+|all)[|]\d+)(?:[|][👍👎❤️🔥🥰👏😁🤔🤯😱🤬😢🎉🤩🤮💩🙏👌🕊🤡🥱🥴😍🐳🌚🌭💯🤣❤️‍🔥]|[|][\u2764])+")
                 ),
+            ),
+            loader.ConfigValue(
+                "reactions_chance",
+                doc=lambda: self.strings("_cfg_doc_reactions_chance"),
+                validator=loader.validators.Series(
+                    validator=loader.validators.RegExp(r"^(?:(?:\d+)[|](?:\d+|global)|(?:\d+|all)[|]\d+)(?:[|](?:0(?:\.\d{1,2})?|1(?:\.0{1,2})?))$")
+                ),
+            ),
+            loader.ConfigValue(
+                "shuffle_reactions",
+                True,
+                doc=lambda: self.strings("_cfg_doc_shuffle"),
+                validator=loader.validators.Boolean(),
             ),
         )
 
@@ -73,22 +110,49 @@ class ApodiktumAutoReactMod(loader.Module):
         """
         This is no command.\nEverything is configured within .config apodiktum autoreact
         """
-        return await utils.answer(message, self.strings("no_command").format(self.strings("developer")))
+        # await message.delete()
+        await self.allmodules.commands["config"](
+            await utils.answer(message, f"{self.get_prefix()}config apodiktum autoreact")
+        )
+        # return await utils.answer(message, self.strings("no_command").format(self.strings("developer")))
 
     async def watcher(self, message: Message):
         if not isinstance(message, Message):
             return
         if not self.config["reaction_active"]:
             return
-        config = self.config["reactions"]
-        for reaction in config:
+        reactions = self.config["reactions"]
+        reactions_chance = self.config["reactions_chance"]
+
+        for reaction in reactions:
             userid, chatid, *emojis = reaction.split("|")
             if userid == "all" and chatid == "global":
                 return
             if (str(message.sender_id) == userid or userid == "all") and (str(utils.get_chat_id(message)) == chatid or chatid == "global"):
+                if not await self._reactions_chance(reactions_chance):
+                    return
+                if self.config["shuffle_reactions"]:
+                    emojis = random.sample(emojis, len(emojis))
+                await self._delay()
                 for emoji in emojis:
                     if await self._react_message(message, emoji, chatid):
                         return
+
+    async def _delay(self):
+        if self.config["global_delay"] and not self.config["random_delay"]:
+            await asyncio.sleep(self.config["global_delay"])
+        elif self.config["global_delay"] and self.config["random_delay"]:
+            await asyncio.sleep(round(random.uniform(0, self.config["global_delay"]), 2))
+
+    async def _reactions_chance(self, reactions_chance):
+        for reactions_chance in reactions_chance:
+            userid, chatid, chance = reactions_chance.split("|")
+            if userid == "all" and chatid == "global":
+                return
+            if (str(message.sender_id) == userid or userid == "all") and (str(utils.get_chat_id(message)) == chatid or chatid == "global"):
+                if random.random() > float(chance):
+                    return False
+        return True
 
     async def _react_message(self, message, emoji, chatid):
         try:
