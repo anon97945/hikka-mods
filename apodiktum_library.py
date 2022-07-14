11ee56d6dd0898c2158e34e65d033bd41f754462
anon97945 <54719502+anon97945@users.noreply.github.com>
2022-07-14 07:29:36 +0200

    update
    * edited library for 1.2.10 native support

--- apodiktum_library.py
+++ apodiktum_library.py
@@ -15,8 +15,6 @@ __version__ = (0, 0, 3)
 
 # meta developer: @apodiktum_modules
 
-# scope: hikka_only
-# scope: hikka_min 1.1.28
 
 import logging
 import telethon
@@ -57,34 +55,15 @@ class ApodiktumLibraryFunctions:
 
 
 @loader.tds
-class ApodiktumLibMod(ApodiktumLibraryFunctions, loader.Module):
-    """
-    This is a Library module required for Apodiktum Modules and also 3rd-party modules.
-    >>Do not unload this!<< 
-    """
+class ApodiktumLib(ApodiktumLibraryFunctions, loader.Library):
+    developer = "@apodiktum_modules"
 
     strings = {
-        "name": "Apo-Library",
         "developer": "@anon97945",
-        "incorrect_language": "🚫 <b>Incorrect language specified.</b>",
-        "lang_saved": "{} <b>forced language saved!</b>",
-        "forced_lang": "<b>Forced language {}!</b>",
     }
 
-    strings_de = {
-        "_cls_doc": ("Dies ist ein Bibliotheksmodul, das für Apodiktum-Module und auch Module von Drittanbietern benötigt wird."
-                     ">>Nicht entfernen!<<"),
-        "_cmd_doc_capolib": "Dadurch wird die Konfiguration für das Modul geöffnet.",
-    }
-
-    strings_ru = {
-        "_cls_doc": ("Это библиотечный модуль, необходимый для модулей Apodiktum, а также для модулей сторонних производителей."
-                     ">>Не удаляйте!<<"),
-        "_cmd_doc_capolib": "Это откроет конфиг для модуля.",
-    }
-
-    def __init__(self):
-        self.ratelimit = []
+    async def init(self):
+        logger.info("%s v%s by %s is init.", self._name, __version__, self.developer)
 
     async def client_ready(self, client, db):
         self._db = db
@@ -93,42 +72,4 @@ class ApodiktumLibMod(ApodiktumLibraryFunctions, loader.Module):
         self._classname = self.__class__.__name__
         self._lib_db = self._db[self._classname]
         self._chats_db = self._lib_db.setdefault("chats", {})
-
-    async def capolibcmd(self, message: Message):
-        """
-        This will open the config for the module.
-        """
-        await self.allmodules.commands["config"](
-            await utils.answer(message, f"{self.get_prefix()}config {self._name}")
-        )
-
-    async def fclcmd(self, message: Message):
-        """
-        force language of modules in this chat.
-        """
-        args = utils.get_args_raw(message)
-        chat_id = utils.get_chat_id(message)
-        chatid_str = str(chat_id)
-        chatid_db = self._chats_db.setdefault(chatid_str, {})
-
-        if not args:
-            if len(args) not in [0, 2]:
-                await utils.answer(message, self.strings("incorrect_language"))
-                return
-            await utils.answer(
-                message,
-                self.strings("forced_lang").format(
-                    utils.get_lang_flag(chatid_db.get("forced_lang").lower() if chatid_db.get("forced_lang").lower() != "en" else "gb")
-                ),
-            )
-            return
-
-        chatid_db.update({"forced_lang": args.lower()})
-        self._db.set(self._classname, "chats", self._chats_db)
-
-        await utils.answer(
-            message,
-            self.strings("lang_saved").format(
-                utils.get_lang_flag(args.lower() if args.lower() != "en" else "gb")
-            ),
-        )
+        logger.info("%s v%s by %s is client_ready.", self._name, __version__, self.developer)
