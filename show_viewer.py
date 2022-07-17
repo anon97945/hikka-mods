48712d65f55f5fbec19eb92f54224308422e3212
anon97945 <54719502+anon97945@users.noreply.github.com>
2022-07-17 10:34:51 +0200

    Revert "Lib test"

--- show_viewer.py
+++ show_viewer.py
@@ -1,4 +1,4 @@
-__version__ = (0, 0, 18)
+__version__ = (0, 0, 15)
 
 
 # ▄▀█ █▄ █ █▀█ █▄ █ █▀█ ▀▀█ █▀█ █ █ █▀
@@ -16,13 +16,16 @@ __version__ = (0, 0, 18)
 # meta developer: @apodiktum_modules
 
 # scope: hikka_only
-# scope: hikka_min 1.2.11
+# scope: hikka_min 1.2.10
+
+from .. import loader, utils
+from telethon.tl.types import Message
 
 import logging
 
-from telethon.tl.types import Message
-
-from .. import loader, utils
+import collections  # for MigratorClass
+import hashlib  # for MigratorClass
+import copy     # for MigratorClass
 
 logger = logging.getLogger(__name__)
 
@@ -43,22 +46,6 @@ class ApodiktumShowViewsMod(loader.Module):
         "no_channel": "No channel set.",
         "no_reply": "You need to reply to a message.",
         "views": "Total <code>{}</code> views.",
-    }
-
-    strings_en = {
-    }
-
-    strings_de = {
-    }
-
-    strings_ru = {
-    }
-
-    all_strings = {
-        "strings": strings,
-        "strings_en": strings,
-        "strings_de": strings_de,
-        "strings_ru": strings_ru,
     }
 
     def __init__(self):
@@ -95,10 +82,31 @@ class ApodiktumShowViewsMod(loader.Module):
     async def client_ready(self, client, db):
         self._db = db
         self._client = client
-        self.apo_lib = await self.import_lib(
-            "https://raw.githubusercontent.com/anon97945/hikka-mods/lib_test/apodiktum_library.py",
-            suspend_on_error=True,
-        )
+        self.base_strings = self.strings._base_strings
+        # MigratorClass
+        self._migrator = MigratorClass()  # MigratorClass define
+        await self._migrator.init(client, db, self, self.__class__.__name__, self.strings("name"), self.config["auto_migrate_log"], self.config["auto_migrate_debug"])  # MigratorClass Initiate
+        await self._migrator.auto_migrate_handler(self.config["auto_migrate"])
+        # MigratorClass
+
+    def _strings(self, string: str, chat_id: int = None):
+        if self.lookup("Apo-Translations") and chat_id:
+            forced_translation_db = self.lookup("Apo-Translations").config
+            strings_en = self.strings_en if getattr(self, "strings_en", False) else {}
+            strings_de = self.strings_de if getattr(self, "strings_de", False) else {}
+            strings_ru = self.strings_ru if getattr(self, "strings_ru", False) else {}
+            languages = {
+                "en_chats": {**self.base_strings, **strings_en},
+                "de_chats": {**self.base_strings, **strings_de},
+                "ru_chats": {**self.base_strings, **strings_ru},
+            }
+            for lang, strings in languages.items():
+                if chat_id in forced_translation_db[lang]:
+                    if string in strings:
+                        return strings[string]
+                    logger.debug(f"String: {string} not found in\n{strings}")
+                    break
+        return self.strings(string)
 
     async def svcmd(self, message: Message):
         """
@@ -108,12 +116,12 @@ class ApodiktumShowViewsMod(loader.Module):
         args = utils.get_args_raw(message)
         msg = None
         if not self.config["channel"]:
-            await utils.answer(message, self.apo_lib.utils.get_str("no_channel", self.all_strings, message))
+            await utils.answer(message, self._strings("no_channel", utils.get_chat_id(message)))
             return
         if message.is_reply:
             msg = await message.get_reply_message()
         elif not args:
-            await utils.answer(message, self.apo_lib.utils.get_str("no_args", self.all_strings, message))
+            await utils.answer(message, self._strings("no_args", utils.get_chat_id(message)))
             return
         await message.delete()
         if message.is_reply and msg.sender_id == self._tg_id:
@@ -133,7 +141,261 @@ class ApodiktumShowViewsMod(loader.Module):
         if message.is_reply:
             msg = await message.get_reply_message()
         else:
-            await utils.answer(message, self.apo_lib.utils.get_str("no_reply", self.all_strings, message))
+            await utils.answer(message, self._strings("no_reply", utils.get_chat_id(message)))
             return
         view_count = msg.views
-        await utils.answer(message, self.apo_lib.utils.get_str("views", self.all_strings, message).format(view_count))
+        await utils.answer(message, self._strings("views", utils.get_chat_id(message)).format(view_count))
+
+
+class MigratorClass():
+    """
+    # ▄▀█ █▄ █ █▀█ █▄ █ █▀█ ▀▀█ █▀█ █ █ █▀
+    # █▀█ █ ▀█ █▄█ █ ▀█ ▀▀█   █ ▀▀█ ▀▀█ ▄█
+    #
+    #              © Copyright 2022
+    #
+    #             developed by @anon97945
+    #
+    #          https://t.me/apodiktum_modules
+    #
+    # 🔒 Licensed under the GNU GPLv3
+    # 🌐 https://www.gnu.org/licenses/gpl-3.0.html
+    """
+
+    strings = {
+        "_log_doc_migrated_db": "Migrated {} database of {} -> {}:\n{}",
+        "_log_doc_migrated_cfgv_val": "[Dynamic={}] Migrated default config value:\n{} -> {}",
+        "_log_doc_no_dynamic_migration": "No module config found. Did not dynamic migrate:\n{{{}: {}}}",
+        "_log_doc_migrated_db_not_found": "`{}` database not found. Did not migrate {} -> {}",
+    }
+
+    changes = {
+        "migration1": {
+            "name": {
+                "old": "Apo ShowViews",
+                "new": "Apo-ShowViews",
+            },
+        },
+    }
+
+    def __init__(self):
+        self._ratelimit = []
+
+    async def init(
+        self,
+        client: "TelegramClient",  # type: ignore
+        db: "Database",  # type: ignore
+        modules: str,  # type: ignore
+        classname: str,  # type: ignore
+        name: str,  # type: ignore
+        log: bool = False,  # type: ignore
+        debug: bool = False,  # type: ignore
+    ):
+        self._client = client
+        self._db = db
+        self._classname = classname
+        self._name = name
+        self.modules = modules
+        self.log = log
+        self.debug = debug
+        self.hashs = []
+        self.hashs = self._db.get(self._classname, "hashs", [])
+        self._migrate_to = list(self.changes)[-1] if self.changes else None
+
+    async def migrate(self, log: bool = False, debug: bool = False):
+        self.log = log
+        self.debug = debug
+        if self._migrate_to is not None:
+            self.hashs = self._db.get(self._classname, "hashs", [])
+
+            migrate = await self.check_new_migration()
+            full_migrated = await self.full_migrated()
+            if migrate:
+                await self._logger(f"Open migrations: {migrate}", self.debug, True)
+                if await self._migrator_func():
+                    await self._logger("Migration done.", self.debug, True)
+                    return True
+            elif not full_migrated:
+                await self.force_set_hashs()
+                await self._logger(f"Open migrations: {migrate} | Forcehash done: {self.hashs}", self.debug, True)
+                return False
+            else:
+                await self._logger(f"Open migrations: {migrate} | Skip migration.", self.debug, True)
+                return False
+            return False
+        await self._logger("No changes in `changes` dictionary found.", self.debug, True)
+        return False
+
+    async def auto_migrate_handler(self, auto_migrate: bool = False):
+        if self._migrate_to is not None:
+            self.hashs = self._db.get(self._classname, "hashs", [])
+            migrate = await self.check_new_migration()
+            full_migrated = await self.full_migrated()
+            if auto_migrate and migrate:
+                await self._logger(f"Open migrations: {migrate} | auto_migrate: {auto_migrate}", self.debug, True)
+                if await self._migrator_func():
+                    await self._logger("Migration done.", self.debug, True)
+                    return
+            elif not auto_migrate and not full_migrated:
+                await self.force_set_hashs()
+                await self._logger(f"Open migrations: {migrate} | auto_migrate: {auto_migrate} | Forcehash done: {self.hashs}", self.debug, True)
+                return
+            else:
+                await self._logger(f"Open migrations: {migrate} | auto_migrate: {auto_migrate} | Skip migrate_handler.", self.debug, True)
+                return
+        await self._logger("No changes in `changes` dictionary found.", self.debug, True)
+        return
+
+    async def force_set_hashs(self):
+        await self._set_missing_hashs()
+        return True
+
+    async def check_new_migration(self):
+        chash = hashlib.sha256(self._migrate_to.encode('utf-8')).hexdigest()
+        return chash not in self.hashs
+
+    async def full_migrated(self):
+        full_migrated = True
+        for migration in self.changes:
+            chash = hashlib.sha256(migration.encode('utf-8')).hexdigest()
+            if chash not in self.hashs:
+                full_migrated = False
+        return full_migrated
+
+    async def _migrator_func(self):
+        for migration in self.changes:
+            chash = hashlib.sha256(migration.encode('utf-8')).hexdigest()
+            if chash not in self.hashs:
+                old_classname, new_classname, old_name, new_name = await self._get_names(migration)
+                for category in self.changes[migration]:
+                    await self._copy_config_init(migration, old_classname, new_classname, old_name, new_name, category)
+                await self._set_hash(chash)
+        return True
+
+    async def _copy_config_init(self, migration, old_classname, new_classname, old_name, new_name, category):
+        if category == "classname":
+            if self._classname != old_classname and (old_classname in self._db.keys() and self._db[old_classname] and old_classname is not None):
+                await self._logger(f"{migration} | {category} | old_value: {str(old_classname)} | new_value: {str(new_classname)}", self.debug, True)
+                await self._copy_config(category, old_classname, new_classname, new_name)
+            else:
+                await self._logger(self.strings["_log_doc_migrated_db_not_found"].format(category, old_classname, new_classname))
+        elif category == "name":
+            await self._logger(f"{migration} | {category} | old_value: {str(old_name)} | new_value: {str(new_name)}", self.debug, True)
+            if self._name != old_name and (old_name in self._db.keys() and self._db[old_name] and old_name is not None):
+                await self._copy_config(category, old_name, new_name, new_classname)
+            else:
+                await self._logger(self.strings["_log_doc_migrated_db_not_found"].format(category, old_name, new_name))
+        elif category == "config":
+            await self._migrate_cfg_values(migration, category, new_name, new_classname)
+        return
+
+    async def _get_names(self, migration):
+        old_name = None
+        old_classname = None
+        new_name = None
+        new_classname = None
+        for category in self.changes[migration]:
+            if category == "classname":
+                old_classname, new_classname = await self._get_changes(self.changes[migration][category].items())
+            elif category == "name":
+                old_name, new_name = await self._get_changes(self.changes[migration][category].items())
+        if not new_name:
+            new_name = self._name
+        if not new_classname:
+            new_classname = self._classname
+        return old_classname, new_classname, old_name, new_name
+
+    @staticmethod
+    async def _get_changes(changes):
+        old_value = None
+        new_value = None
+        for state, value in changes:
+            if state == "old":
+                old_value = value
+            elif state == "new":
+                new_value = value
+        return old_value, new_value
+
+    async def _migrate_cfg_values(self, migration, category, new_name, new_classname):
+        if new_classname in self._db.keys() and "__config__" in self._db[new_classname]:
+            if configdb := self._db[new_classname]["__config__"]:
+                for cnfg_key in self.changes[migration][category]:
+                    old_value, new_value = await self._get_changes(self.changes[migration][category][cnfg_key].items())
+                    for key, value in configdb.items():
+                        await self._logger(f"{migration} | {category} | ({{old_value: {str(old_value)}}} `==` {{new_value: {str(value)}}}) `and` {{key: {key}}} `==` {{cnfg_key: {cnfg_key}}}", self.debug, True)
+                        if value == old_value and key == cnfg_key:
+                            dynamic = False
+                            self._db[new_classname]["__config__"][cnfg_key] = new_value
+                            if (
+                                self.modules.lookup(new_name)
+                                and self.modules.lookup(new_name).config
+                                and key in self.modules.lookup(new_name).config
+                            ):
+                                self.modules.lookup(new_name).config[cnfg_key] = new_value
+                                dynamic = True
+                            await self._logger(self.strings["_log_doc_migrated_cfgv_val"].format(dynamic, value, new_value))
+        return
+
+    async def _copy_config(self, category, old_name, new_name, name):
+        if self._db[new_name]:
+            temp_db = {new_name: copy.deepcopy(self._db[new_name])}
+            self._db[new_name].clear()
+            self._db[new_name] = await self._deep_dict_merge(temp_db[new_name], self._db[old_name])
+            temp_db.pop(new_name)
+        else:
+            self._db[new_name] = copy.deepcopy(self._db[old_name])
+        self._db.pop(old_name)
+        await self._logger(self.strings["_log_doc_migrated_db"].format(category, old_name, new_name, self._db[new_name]))
+        if category == "classname":
+            await self._make_dynamic_config(name, new_name)
+        if category == "name":
+            await self._make_dynamic_config(new_name, name)
+        return
+
+    async def _deep_dict_merge(self, dct1, dct2, override=True) -> dict:
+        merged = copy.deepcopy(dct1)
+        for k, v2 in dct2.items():
+            if k in merged:
+                v1 = merged[k]
+                if isinstance(v1, dict) and isinstance(v2, collections.abc.Mapping):
+                    merged[k] = await self._deep_dict_merge(v1, v2, override)
+                elif isinstance(v1, list) and isinstance(v2, list):
+                    merged[k] = v1 + v2
+                elif override:
+                    merged[k] = copy.deepcopy(v2)
+            else:
+                merged[k] = copy.deepcopy(v2)
+        return merged
+
+    async def _make_dynamic_config(self, new_name, new_classname=None):
+        if new_classname is None:
+            return
+        if "__config__" in self._db[new_classname].keys():
+            for key, value in self._db[new_classname]["__config__"].items():
+                if (
+                    self.modules.lookup(new_name)
+                    and self.modules.lookup(new_name).config
+                    and key in self.modules.lookup(new_name).config
+                ):
+                    self.modules.lookup(new_name).config[key] = value
+                else:
+                    await self._logger(self.strings["_log_doc_no_dynamic_migration"].format(key, value))
+        return
+
+    async def _set_hash(self, chash):
+        self.hashs.append(chash)
+        self._db.set(self._classname, "hashs", self.hashs)
+        return
+
+    async def _set_missing_hashs(self):
+        for migration in self.changes:
+            chash = hashlib.sha256(migration.encode('utf-8')).hexdigest()
+            if chash not in self.hashs:
+                await self._set_hash(chash)
+
+    async def _logger(self, log_string, debug: bool = False, debug_msg: bool = False):
+        if not debug_msg and self.log:
+            return logger.info(log_string)
+        if debug and debug_msg:
+            return logger.info(log_string)
+        return logger.debug(log_string)
